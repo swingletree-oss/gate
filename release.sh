3880bbc69164326baa64663bfd9a1a19d73d56e2
error418 <error418@users.noreply.github.com>
2019-11-10 17:39:20 +0100

    fix(release): fix release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker login docker.pkg.github.com -u error418 -p $GITHUB_PKG_TOKEN $ && \
+docker login -u error418 -p $GITHUB_PKG_TOKEN docker.pkg.github.com && \
 docker build . --file Dockerfile --build-arg GITHUB_PKG_TOKEN=$GITHUB_PKG_TOKEN --tag docker.pkg.github.com/swingletree-oss/gate:$1 && \
 docker push docker.pkg.github.com/swingletree-oss/gate:$1
